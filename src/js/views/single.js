2586fce961b69ab38a360a151cff0e32483126ce
NVR-2023 <123973723+NVR-2023@users.noreply.github.com>
2023-07-21 14:31:49 +0000

    changes to single

--- src/js/views/single.js
+++ src/js/views/single.js
@@ -5,7 +5,7 @@ import { Context } from "../store/appContext";
 export const Single = () => {
   const { store, actions } = useContext(Context);
   const { id } = useParams();
-  const [singleCharacter, setSingleCharacter] = useState([]); // use state to store the fetched character data
+  const [singleCharacter, setSingleCharacter] = useState([]);
 
   const fetchCharacter = async (url) => {
     try {
@@ -15,31 +15,31 @@ export const Single = () => {
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   useEffect(() => {
     fetchCharacter(`https://rickandmortyapi.com/api/character/${id}`);
   }, [id]);
 
   return (
-    <div>
-      {singleCharacter ? (
-        <div>
-          <h1 className="display">{singleCharacter.name}</h1>
-          <img src={singleCharacter.image} alt={singleCharacter.name} />
-          {
-            Object.keys(singleCharacter).map(key => {
+    <div className="container d-flex justify-content-center"> 
+      <div className="row">
+        {singleCharacter ? (
+          <div>
+            <h1 className="display">{singleCharacter.name}</h1>
+            <img src={singleCharacter.image} alt={singleCharacter.name} />
+            {Object.keys(singleCharacter).map((key) => {
               if (typeof singleCharacter[key] === "string") {
                 return <p key={key}>{singleCharacter[key]}</p>;
               } else {
                 return null;
               }
-            })
-          }
-        </div>
-      ) : (
-        <p>Loading character...</p>
-      )}
+            })}
+          </div>
+        ) : (
+          <p>Loading character...</p>
+        )}
+      </div>
     </div>
   );
 };
